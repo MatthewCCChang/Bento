303540198a4fc6f0e84bb63f93b86f8ce8ade5ad
MatthewCCChang <deadwalker1228@gmail.com>
2026-02-05 17:11:59 +0800

    Basic db connection creation

--- backend/internal/db/create.go
+++ backend/internal/db/create.go
@@ -1,8 +1,22 @@
 package db
 
-// import (
-// 	"context"
-// 	"database/sql"
-// 	"fmt"
-// )
+import (
+	"context"
+	"os"
+	"fmt"
 
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateConnectionPool(connections int) (*pgxpool.Pool, error) {
+	dbURL := os.Getenv("DB_URL")
+	config, err := pgxpool.ParseConfig(dbURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to parse DATABASE_URL: %v\n", err)
+		os.Exit(1)
+	}
+	config.MaxConns = int32(connections)
+
+	conn, err := pgxpool.NewWithConfig(context.Background(), config)
+	return conn, nil
+}
